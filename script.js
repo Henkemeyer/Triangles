e75552f8e814347912836faa1adc0b59a94d58a4
Henkemeyer <henkemeyert5137@my.uwstout.edu>
2017-06-05 19:48:44 -0500

    Problem #2
    added ability to find a triangle

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 var a_canvas = document.getElementById("a");
 var context = a_canvas.getContext("2d");
 
@@ -23,5 +22,42 @@ for (i = 0; i < 6; i++) {
         context.stroke();
         context.fillStyle = "blue";
         context.fill();
+    }
+}
+
+function verticesCalculation() {
+    var txt;
+    var answer;
+    var x = [0,0,0];
+    var y = [0,0,0];
+    var column;
+    var row;
+    var position;
+    var vertices = prompt("Please enter the vertices:", "[,][,][,]");
+    if (vertices == null || vertices == "") {
+        txt = "User cancelled the prompt.";
+    } else {
+        for (a = 0; a < 3; a++) { 
+            position = vertices.indexOf(",");
+            x[a] = vertices.substring(1, position);
+            vertices = vertices.substring(position+1);
+            position = vertices.indexOf("]");
+            y[a] = vertices.substring(0, position);
+            vertices = vertices.substring(position+1);
+        }
+        x.sort
+        column = (Number(x[1])+Number(x[2]))/10
+        
+        var toty = (Number(y[0])+Number(y[1])+Number(y[2]))/10
+        
+        if (toty < 3) { row = "A"}
+        else if (toty < 6) { row = "B"}
+        else if (toty < 9) { row = "C"}
+        else if (toty < 12) { row = "D"}
+        else if (toty < 15) { row = "E"}
+        else if (toty < 18) { row = "F"}
+        
+        answer = "Your Triangle is "+row+column
+        window.alert(answer);
     }
 }
